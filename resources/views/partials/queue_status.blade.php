89d04d334560a8e71c42239de186503443950b7c
artyfarty <mail@artyfarty.ru>
2020-09-24 19:10:09 +0300

    статус показанности в партиале очереди

--- resources/views/partials/queue_status.blade.php
+++ resources/views/partials/queue_status.blade.php
@@ -1,4 +1,11 @@
 @foreach($notification->queued as $queue_entry)
-    <p>{{ veryshortdatetime($queue_entry->display_at) }}</p>
+    <p>
+        {{ veryshortdatetime($queue_entry->display_at) }}
+        @if ($queue_entry->was_displayed)
+            <span class="badge badge-success">Показано</span>
+        @else
+            <span class="badge">Не показано</span>
+        @endif
+    </p>
 @endforeach
 
