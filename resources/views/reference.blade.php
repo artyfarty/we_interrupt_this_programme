746b1448f528ba951ec8e842f538f4dc163b69aa
artyfarty <mail@artyfarty.ru>
2020-09-24 23:12:00 +0300

    поправил бллок с референсом

---
+++ resources/views/reference.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<h2>API</h2>
+<p><code>{{ url("/api/poll/{$pwd}") }}</code> – получить сообщение и пометить показанным</p>
+<p><code>{{ url("/api/poll/{$pwd}/0") }}</code> – получить сообщение и не помечать показанным</p>
+<p><code>{{ url("/api/ack/__ID__/{$pwd}") }}</code> – пометить сообщение показанным</p>
+<p><code>{{ url("/announcer-app") }}/</code> – виджет OBS</p>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,5 @@
     <p>А затем перенаполни базу с их учетом <code>php artisan migrate:fresh --seed</code></p>
     <p>Если страница похожа на говно без стилей то сделвй <code>npm run dev</code></p>
 
-    <h2>API</h2>
-    <p><code>{{ url('/api/poll/WITP_PASSWORD') }}</code> – получить сообщение и пометить показанным</p>
-    <p><code>{{ url('/api/poll/WITP_PASSWORD/0') }}</code> – получить сообщение и не помечать показанным</p>
-    <p><code>{{ url('/api/ack/__ID__/WITP_PASSWORD') }}</code> – пометить сообщение показанным</p>
+    <a href="{{ route("reference") }}">Апи методы и прочее</a>
 @endsection
